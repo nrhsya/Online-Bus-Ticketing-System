ac339eeb4fa93ddea63f6c48b963fde6045d19d7
FatihahLaina <107554039+FatihahLaina@users.noreply.github.com>
2022-06-19 14:07:35 +0800

    ina

--- app/src/main/java/com/hasya/onlinebusticketingsystem/SelectSeat.java
+++ app/src/main/java/com/hasya/onlinebusticketingsystem/SelectSeat.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
@@ -13,103 +15,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 public class SelectSeat extends AppCompatActivity {
-
-    GridLayout mainGrid;
-    Double seatPrice = 27.00;
-    Double totalCost = 0.0;
-    int totalSeats = 0;
-    TextView totalPrice;
-    TextView totalBookedSeats;
-    private Button buttonBook;
-    private int seatGaping;
+    Button btn;
 
     // private FirebaseAuth firebaseAuth;
     //  private ProgressDialog progressDialog;
     // private DatabaseReference databaseReference;
 
+
+    //  firebaseAuth= FirebaseAuth.getInstance();
+    // databaseReference = FirebaseDatabase.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_seat);
 
-        getSupportActionBar().setTitle("Select Your Favorite Seats");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.activity_select_seat);
 
-        //  firebaseAuth= FirebaseAuth.getInstance();
-        // databaseReference = FirebaseDatabase.getInstance().getReference();
-
-       /* LinearLayout layoutSeat = new LinearLayout(this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutSeat.setOrientation(LinearLayout.VERTICAL);
-        layoutSeat.setLayoutParams(params);
-        layoutSeat.setPadding(8 * seatGaping, 8 *  seatGaping, 8 * seatGaping, 8 *  seatGaping);
-        layoutSeat.addView(layoutSeat);
-
-        LinearLayout layout = null;
-
-        int count = 0;
-*/
-
-       // mainGrid = (GridLayout) findViewById(R.id.mainGrid);
-        totalBookedSeats = (TextView) findViewById(R.id.total_seats);
-        totalPrice = (TextView) findViewById(R.id.total_cost);
-       // buttonBook = (Button) findViewById(R.id.btnBook);
-
-        //Set Event
-        setToggleEvent(mainGrid);
-
-
-        buttonBook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String totalPriceI=totalPrice.getText().toString().trim();
-                String totalBookedSeatsI=totalBookedSeats.getText().toString().trim();
-
-                PaymentDetail paymentDetail=new PaymentDetail(totalPriceI,totalBookedSeatsI);
-
-                //  FirebaseUser user=firebaseAuth.getCurrentUser();
-                //  databaseReference.child(user.getUid()).child("SeatDetails").setValue(paymentDetail);
-
-                Intent intent=new Intent(SelectSeat.this,InfoForm.class);
-                intent.putExtra("TOTALCOST",totalPriceI);
-                intent.putExtra("TOTALSEAT",totalBookedSeatsI);
-
-
-                startActivity(intent);
-
-            }
-        });
-
+            btn = findViewById(R.id.btnSubmit);
+       btn.setOnClickListener(v -> openPaymentMainPage());
     }
 
+    public void openPaymentMainPage(){
+        Intent intent = new Intent(this, PaymentMainpage.class);
+        startActivity(intent);
+    }
+    }
 
-    private void setToggleEvent(GridLayout mainGrid) {
-        //Loop all child item of Main Grid
-        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-            //You can see , all child item is CardView , so we just cast object to CardView
-            final CardView cardView = (CardView) mainGrid.getChildAt(i);
-            final int finalI = i;
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#00FF00"));
-                        totalCost += seatPrice;
-                        ++totalSeats;
-                        Toast.makeText(SelectSeat.this, "You Selected Seat Number :" + (finalI + 1), Toast.LENGTH_SHORT).show();
-
-                    } else {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
-                        totalCost -= seatPrice;
-                        --totalSeats;
-                        Toast.makeText(SelectSeat.this, "You Unselected Seat Number :" + (finalI + 1), Toast.LENGTH_SHORT).show();
-                    }
-                    totalPrice.setText("" + totalCost+"0");
-                    totalBookedSeats.setText("" + totalSeats);
-                }
-            });
-        }
-    }}
